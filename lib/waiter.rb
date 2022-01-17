4cd0d95210d4fdb6763a426cf53117ac3906809c
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-01-17 15:32:31 +1030

    Added body to classes

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
+    attr_accessor :name, :exp
+    @@all = []
+    def self.all 
+        @@all
+    end
+
+    def initialize(name, exp)
+        @name = name
+        @exp = exp
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self }
+    end 
+
+    def best_tipper
+        meal = meals.max_by {|meal| meal.tip}
+        meal.customer
+    end
 
 end
